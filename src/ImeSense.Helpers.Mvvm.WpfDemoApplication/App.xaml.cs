bf3727b2dd1c3f027de336a3db6ac7d0728cbd54
acidicMercury8 <acidicmercury8@outlook.com>
2022-09-14 22:12:42 +0300

    Add empty WPF demo project

---
+++ src/ImeSense.Helpers.Mvvm.WpfDemoApplication/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Windows;
+
+namespace ImeSense.Helpers.Mvvm.WpfDemoApplication {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
