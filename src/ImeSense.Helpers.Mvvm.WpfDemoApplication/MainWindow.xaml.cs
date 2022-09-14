3de85ada84a7a4c579f0215dca0d86203b66c40a
acidicMercury8 <acidicmercury8@outlook.com>
2022-09-14 22:12:42 +0300

    Add empty WPF demo project

---
+++ src/ImeSense.Helpers.Mvvm.WpfDemoApplication/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace ImeSense.Helpers.Mvvm.WpfDemoApplication {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
