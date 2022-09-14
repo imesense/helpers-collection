3701232db5d882e476c969fb4fc348c75a6333ba
acidicMercury8 <acidicmercury8@outlook.com>
2022-09-14 22:42:41 +0300

    Redefine entry point of WPF demo project

--- src/ImeSense.Helpers.Mvvm.WpfDemoApplication/App.xaml.cs
+++ src/ImeSense.Helpers.Mvvm.WpfDemoApplication/App.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
+
+using ImeSense.Helpers.Mvvm.WpfDemoApplication.Views;
 
 namespace ImeSense.Helpers.Mvvm.WpfDemoApplication {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs eventArgs) {
+            base.OnStartup(eventArgs);
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
