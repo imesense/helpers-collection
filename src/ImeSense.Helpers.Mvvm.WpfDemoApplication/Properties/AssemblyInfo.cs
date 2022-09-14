c2a26225e7e540b1b16deeb2fc007fd1e58aa2b2
acidicMercury8 <acidicmercury8@outlook.com>
2022-09-14 22:32:55 +0300

    Configure WPF demo project

--- src/ImeSense.Helpers.Mvvm.WpfDemoApplication/Properties/AssemblyInfo.cs
+++ src/ImeSense.Helpers.Mvvm.WpfDemoApplication/Properties/AssemblyInfo.cs
@@ -10,9 +10,9 @@ using System.Windows;
 [assembly: AssemblyTitle("ImeSense.Helpers.Mvvm.WpfDemoApplication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ImeSense.Helpers.Mvvm.WpfDemoApplication")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("ImeSense")]
+[assembly: AssemblyProduct("ImeSense.Helpers")]
+[assembly: AssemblyCopyright("Copyright (c) 2022 ImeSense, Renat Shakirov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -46,5 +46,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
