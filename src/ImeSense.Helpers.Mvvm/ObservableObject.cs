bf7b259574a3d19bc93d03c2e92c12f4793dbf77
acidicMercury8 <acidicmercury8@outlook.com>
2022-09-17 20:03:53 +0300

    Implement `INotifyPropertyChanging` for `ObservableObject`

--- src/ImeSense.Helpers.Mvvm/ObservableObject.cs
+++ src/ImeSense.Helpers.Mvvm/ObservableObject.cs
@@ -2,8 +2,10 @@
 using System.ComponentModel;
 
 namespace ImeSense.Helpers.Mvvm {
-    public abstract class ObservableObject : INotifyPropertyChanged {
+    public abstract class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public event PropertyChangingEventHandler PropertyChanging;
 
         protected virtual void OnPropertyChanged(PropertyChangedEventArgs eventArgs) {
             var propertyChanged = this.PropertyChanged;
@@ -12,8 +14,19 @@ namespace ImeSense.Helpers.Mvvm {
             }
         }
 
+        protected virtual void OnPropertyChanging(PropertyChangingEventArgs eventArgs) {
+            var propertyChanging = this.PropertyChanging;
+            if (propertyChanging != null) {
+                propertyChanging(this, eventArgs);
+            }
+        }
+
         protected void OnPropertyChanged(string propertyName = null) {
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void OnPropertyChanging(string propertyName = null) {
+            OnPropertyChanging(new PropertyChangingEventArgs(propertyName));
         }
     }
 }
