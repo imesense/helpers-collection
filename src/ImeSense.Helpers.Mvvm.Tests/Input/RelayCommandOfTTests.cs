52214cd8bba7f216f7dce75c3321e2208ac69d65
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-07 18:21:10 +0300

    Add tests for `RelayCommand<T>`

---
+++ src/ImeSense.Helpers.Mvvm.Tests/Input/RelayCommandOfTTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ImeSense.Helpers.Mvvm.Input;
+
+namespace ImeSense.Helpers.Mvvm.Tests.Input {
+    [TestClass]
+    public class RelayCommandOfTTests {
+        public static void ThrowArgumentException(Action action, string parameterName) {
+            var success = false;
+
+            try {
+                action();
+            } catch (Exception exception) {
+                Assert.IsTrue(exception.GetType() == typeof(ArgumentException));
+                Assert.AreEqual(parameterName, ((ArgumentException) exception).ParamName);
+
+                success = true;
+            }
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Execute_Null_ThrowArgumentNullException() {
+            var command = new RelayCommand<string>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CanExecute_Null_ThrowArgumentNullException() {
+            var text = string.Empty;
+            var command = new RelayCommand<string>(source => text = source, null);
+        }
+
+        [TestMethod]
+        public void Execute_String_CorrectStringAssignment() {
+            var text = string.Empty;
+            var command = new RelayCommand<string>(source => text = source);
+            Assert.IsTrue(command.CanExecute("Text"));
+            Assert.IsTrue(command.CanExecute(null));
+
+            ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
+            ThrowArgumentException(() => command.CanExecute(42), "parameter");
+
+            ThrowArgumentException(() => command.Execute(new object()), "parameter");
+            ThrowArgumentException(() => command.Execute(42), "parameter");
+
+            var commandSender = default(object);
+            var commandEventArgs = default(EventArgs);
+            command.CanExecuteChanged += (sender, eventArgs) => {
+                commandSender = sender;
+                commandEventArgs = eventArgs;
+            };
+            command.NotifyCanExecuteChanged();
+            Assert.AreSame(commandSender, command);
+            Assert.AreSame(commandEventArgs, EventArgs.Empty);
+
+            command.Execute((object) "Hello");
+            Assert.AreEqual(text, "Hello");
+
+            command.Execute(null);
+            Assert.AreEqual(text, null);
+        }
+
+        [TestMethod]
+        public void CanExecute_DoubleString_CorrectStringsAssignment() {
+            var text = string.Empty;
+            var command = new RelayCommand<string>(source => text = source, source => source != null);
+            Assert.IsTrue(command.CanExecute("Text"));
+            Assert.IsFalse(command.CanExecute(null));
+
+            ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
+            ThrowArgumentException(() => command.CanExecute(42), "parameter");
+
+            ThrowArgumentException(() => command.Execute(new object()), "parameter");
+            ThrowArgumentException(() => command.Execute(42), "parameter");
+
+            command.Execute((object) "Hello");
+            Assert.AreEqual(text, "Hello");
+
+            command.Execute(null);
+            Assert.IsNull(text);
+        }
+
+        [TestMethod]
+        public void CanExecute_Number_CorrectStringsAssignment() {
+            var n = 0;
+            var command = new RelayCommand<int>(i => n = i);
+            Assert.IsFalse(command.CanExecute(null));
+
+            ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
+            ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
+
+            ThrowArgumentException(() => command.Execute(null), "parameter");
+            ThrowArgumentException(() => command.Execute("Hello"), "parameter");
+            ThrowArgumentException(() => command.Execute(3.14f), "parameter");
+        }
+
+        [TestMethod]
+        public void CanExecute_DoubleNumber_CorrectNumbersAssignment() {
+            var n = 0;
+            var command = new RelayCommand<int>(i => n = i, i => i > 0);
+            Assert.IsFalse(command.CanExecute(null));
+
+            ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
+            ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
+
+            ThrowArgumentException(() => command.Execute(null), "parameter");
+            ThrowArgumentException(() => command.Execute("Hello"), "parameter");
+            ThrowArgumentException(() => command.Execute(3.14f), "parameter");
+        }
+    }
+}
