fb562646c0e2a6a2b141279ba9911f9da9d05bdd
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-07 19:58:44 +0300

    Add documenting comments for MVVM helper

--- src/ImeSense.Helpers.Mvvm/ComponentModel/ObservableObject.cs
+++ src/ImeSense.Helpers.Mvvm/ComponentModel/ObservableObject.cs
@@ -1,11 +1,24 @@
 ﻿using System.ComponentModel;
 
 namespace ImeSense.Helpers.Mvvm.ComponentModel {
+    /// <summary>
+    /// Base class for objects whose properties must be observable
+    /// </summary>
     public abstract class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging {
+        /// <summary>
+        /// Raises when the property value is changed
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// Raises when the property value changes
+        /// </summary>
         public event PropertyChangingEventHandler PropertyChanging;
 
+        /// <summary>
+        /// Raises <see cref="PropertyChanged" /> event
+        /// </summary>
+        /// <param name="eventArgs">Input <see cref="PropertyChangedEventArgs" /> instance</param>
         protected virtual void OnPropertyChanged(PropertyChangedEventArgs eventArgs) {
             var propertyChanged = PropertyChanged;
             if (propertyChanged != null) {
@@ -13,6 +26,10 @@ namespace ImeSense.Helpers.Mvvm.ComponentModel {
             }
         }
 
+        /// <summary>
+        /// Raises the <see cref="PropertyChanging" /> event
+        /// </summary>
+        /// <param name="eventArgs">Input <see cref="PropertyChangingEventArgs" /> instance</param>
         protected virtual void OnPropertyChanging(PropertyChangingEventArgs eventArgs) {
             var propertyChanging = PropertyChanging;
             if (propertyChanging != null) {
@@ -20,10 +37,18 @@ namespace ImeSense.Helpers.Mvvm.ComponentModel {
             }
         }
 
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged" /> event
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed</param>
         protected void OnPropertyChanged(string propertyName = null) {
             OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
         }
 
+        /// <summary>
+        /// Raises the <see cref="PropertyChanging" /> event
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changes</param>
         protected void OnPropertyChanging(string propertyName = null) {
             OnPropertyChanging(new PropertyChangingEventArgs(propertyName));
         }
--- src/ImeSense.Helpers.Mvvm/Input/RelayCommand.cs
+++ src/ImeSense.Helpers.Mvvm/Input/RelayCommand.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 
 namespace ImeSense.Helpers.Mvvm.Input {
+    /// <summary>
+    /// Command to relay its functionality to other objects by invoking delegates
+    /// </summary>
     public class RelayCommand : IRelayCommand {
         /// <summary>
         /// <see cref="Action" /> delegate for invoke on using
@@ -14,8 +17,16 @@ namespace ImeSense.Helpers.Mvvm.Input {
         /// </summary>
         private readonly Func<bool> _canExecute;
 
+        /// <summary>
+        /// Raises on changes whether the command should be executed or not
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        /// Initializes new instance of <see cref="RelayCommand" /> class that can always execute
+        /// </summary>
+        /// <param name="execute">Execution logic</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="execute" /> is <see langword="null" /></exception>
         public RelayCommand(Action execute) {
             if (execute == null) {
                 throw new ArgumentNullException("execute", "Delegate \"execute\" can not be null!");
@@ -24,6 +35,12 @@ namespace ImeSense.Helpers.Mvvm.Input {
             _execute = execute;
         }
 
+        /// <summary>
+        /// Initializes new instance of <see cref="RelayCommand" /> class
+        /// </summary>
+        /// <param name="execute">Execution logic</param>
+        /// <param name="canExecute">Execution status</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="execute" /> or <paramref name="canExecute" /> are <see langword="null" /></exception>
         public RelayCommand(Action execute, Func<bool> canExecute) {
             if (execute == null) {
                 throw new ArgumentNullException("execute", "Delegate \"execute\" can not be null!");
@@ -36,6 +53,11 @@ namespace ImeSense.Helpers.Mvvm.Input {
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
+        /// <returns><see langword="true" /> if this command can be executed<br /><see langword="false" /> otherwise</returns>
         public bool CanExecute(object parameter) {
             var func = _canExecute;
             if (func == null) {
@@ -44,10 +66,18 @@ namespace ImeSense.Helpers.Mvvm.Input {
             return func();
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
         public void Execute(object parameter) {
             _execute();
         }
 
+        /// <summary>
+        /// Notifies of changes in
+        /// <see cref="CanExecute(object)" />
+        /// </summary>
         public void NotifyCanExecuteChanged() {
             var canExecuteChanged = CanExecuteChanged;
             if (canExecuteChanged != null) {
@@ -56,13 +86,33 @@ namespace ImeSense.Helpers.Mvvm.Input {
         }
     }
 
+    /// <summary>
+    /// Generic command to relay its functionality to other objects by invoking delegates
+    /// </summary>
+    /// <typeparam name="T">Type of parameter being passed as input to the callbacks</typeparam>
     public class RelayCommand<T> : IRelayCommand<T> {
+        /// <summary>
+        /// <see cref="Action{T}" /> delegate for invoke on using
+        /// <see cref="Execute(T)" />
+        /// </summary>
         private readonly Action<T> _execute;
 
+        /// <summary>
+        /// Optional <see cref="Predicate{T}" /> delegate for invoke on using
+        /// <see cref="CanExecute(T)" />
+        /// </summary>
         private readonly Predicate<T> _canExecute;
 
+        /// <summary>
+        /// Raises on changes whether the command should be executed or not
+        /// </summary>
         public event EventHandler CanExecuteChanged;
 
+        /// <summary>
+        /// Initializes new instance of <see cref="RelayCommand{T}" /> class
+        /// </summary>
+        /// <param name="execute">Execution logic</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="execute" /> is <see langword="null" /></exception>
         public RelayCommand(Action<T> execute) {
             if (execute == null) {
                 throw new ArgumentNullException("execute", "Delegate \"execute\" can not be null!");
@@ -71,6 +121,12 @@ namespace ImeSense.Helpers.Mvvm.Input {
             _execute = execute;
         }
 
+        /// <summary>
+        /// Initializes new instance of <see cref="RelayCommand{T}" /> class
+        /// </summary>
+        /// <param name="execute">Execution logic</param>
+        /// <param name="canExecute">Execution status</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="execute" /> or <paramref name="canExecute" /> are <see langword="null" /></exception>
         public RelayCommand(Action<T> execute, Predicate<T> canExecute) {
             if (execute == null) {
                 throw new ArgumentNullException("execute", "Delegate \"execute\" can not be null!");
@@ -83,6 +139,10 @@ namespace ImeSense.Helpers.Mvvm.Input {
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Notifies of changes in
+        /// <see cref="CanExecute(object)" />
+        /// </summary>
         public void NotifyCanExecuteChanged() {
             var canExecuteChanged = CanExecuteChanged;
             if (canExecuteChanged != null) {
@@ -90,6 +150,12 @@ namespace ImeSense.Helpers.Mvvm.Input {
             }
         }
 
+        /// <summary>
+        /// Tries to get command argument of compatible type <typeparamref name="T" /> from input <see cref="object" />
+        /// </summary>
+        /// <param name="parameter">Input parameter</param>
+        /// <param name="result">Resulting <typeparamref name="T" /> value</param>
+        /// <returns>Result of checking whether it is possible to get a compatible command argument or not</returns>
         internal static bool TryGetCommandArgument(object parameter, out T result) {
             if (parameter == null && default(T) == null) {
                 result = default(T);
@@ -105,6 +171,11 @@ namespace ImeSense.Helpers.Mvvm.Input {
             return false;
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
+        /// <returns><see langword="true" /> if this command can be executed<br /><see langword="false" /> otherwise</returns>
         public bool CanExecute(T parameter) {
             var predicate = _canExecute;
             if (predicate == null) {
@@ -113,6 +184,11 @@ namespace ImeSense.Helpers.Mvvm.Input {
             return predicate(parameter);
         }
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
+        /// <returns><see langword="true" /> if this command can be executed<br /><see langword="false" /> otherwise</returns>
         public bool CanExecute(object parameter) {
             if (parameter == null && default(T) != null) {
                 return false;
@@ -126,10 +202,18 @@ namespace ImeSense.Helpers.Mvvm.Input {
             return CanExecute(result);
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
         public void Execute(T parameter) {
             _execute(parameter);
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked
+        /// </summary>
+        /// <param name="parameter">Data used by the command</param>
         public void Execute(object parameter) {
             T result;
             if (!TryGetCommandArgument(parameter, out result)) {
