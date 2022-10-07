a09db458704fa22c829896d6c9fd1a77918b584a
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-07 18:03:34 +0300

    Add correct generics `IRelayCommand<T>` and `RelayCommand<T>`

--- src/ImeSense.Helpers.Mvvm/Input/RelayCommand.cs
+++ src/ImeSense.Helpers.Mvvm/Input/RelayCommand.cs
@@ -55,4 +55,87 @@ namespace ImeSense.Helpers.Mvvm.Input {
             }
         }
     }
+
+    public class RelayCommand<T> : IRelayCommand<T> {
+        private readonly Action<T> _execute;
+
+        private readonly Predicate<T> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<T> execute) {
+            if (execute == null) {
+                throw new ArgumentNullException();
+            }
+
+            _execute = execute;
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute) {
+            if (execute == null) {
+                throw new ArgumentNullException("Delegate \"execute\" can not be null!");
+            }
+            if (canExecute == null) {
+                throw new ArgumentNullException("Delegate \"execute\" can not be null!");
+            }
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public void NotifyCanExecuteChanged() {
+            var canExecuteChanged = CanExecuteChanged;
+            if (canExecuteChanged != null) {
+                canExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+
+        internal static bool TryGetCommandArgument(object parameter, out T result) {
+            if (parameter == null && default(T) == null) {
+                result = default(T);
+                return true;
+            }
+
+            if (parameter is T) {
+                T val = (result = (T)parameter);
+                return true;
+            }
+
+            result = default(T);
+            return false;
+        }
+
+        public bool CanExecute(T parameter) {
+            var predicate = _canExecute;
+            if (predicate == null) {
+                return true;
+            }
+            return predicate(parameter);
+        }
+
+        public bool CanExecute(object parameter) {
+            if (parameter == null && default(T) != null) {
+                return false;
+            }
+
+            T result;
+            if (!TryGetCommandArgument(parameter, out result)) {
+                throw new ArgumentException("Parameter \"parameter\" can not be null!", "parameter");
+            }
+
+            return CanExecute(result);
+        }
+
+        public void Execute(T parameter) {
+            _execute(parameter);
+        }
+
+        public void Execute(object parameter) {
+            T result;
+            if (!TryGetCommandArgument(parameter, out result)) {
+                throw new ArgumentException("Parameter \"parameter\" can not be null!", "parameter");
+            }
+            Execute(result);
+        }
+    }
 }
