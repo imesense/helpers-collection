624730000965fe2a37487581d471b615db568885
acidicMercury8 <acidicmercury8@outlook.com>
2022-10-13 19:32:53 +0300

    Fix codestyle of MVVM helper tests

--- src/ImeSense.Helpers.Mvvm.Tests/ComponentModel/ObservableObjectTests.cs
+++ src/ImeSense.Helpers.Mvvm.Tests/ComponentModel/ObservableObjectTests.cs
@@ -4,155 +4,155 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using ImeSense.Helpers.Mvvm.ComponentModel;
 
-namespace ImeSense.Helpers.Mvvm.Tests.ComponentModel {
-    /// <summary>
-    /// Test cases for <see cref="ObservableObject" />
-    /// </summary>
-    [TestClass]
-    public class ObservableObjectTests {
-        public class SimpleModel<T> : ObservableObject {
-            private T? _data;
+namespace ImeSense.Helpers.Mvvm.Tests.ComponentModel;
 
-            public T? Data {
-                get => _data;
-                set => SetProperty(ref _data, value);
-            }
+/// <summary>
+/// Test cases for <see cref="ObservableObject" />
+/// </summary>
+[TestClass]
+public class ObservableObjectTests {
+    public class SimpleModel<T> : ObservableObject {
+        private T? _data;
+
+        public T? Data {
+            get => _data;
+            set => SetProperty(ref _data, value);
         }
+    }
 
-        [TestMethod]
-        public void UpdateEventMethods_SimpleModel_CorrectUpdateArguments() {
-            var model = new SimpleModel<int>();
+    [TestMethod]
+    public void UpdateEventMethods_SimpleModel_CorrectUpdateArguments() {
+        var model = new SimpleModel<int>();
 
-            var changingEventArgs = default(PropertyChangingEventArgs);
-            var changingValue = default(int);
+        var changingEventArgs = default(PropertyChangingEventArgs);
+        var changingValue = default(int);
 
-            var changedEventArgs = default(PropertyChangedEventArgs);
-            var changedValue = default(int);
+        var changedEventArgs = default(PropertyChangedEventArgs);
+        var changedValue = default(int);
 
-            model.PropertyChanging += (sender, eventArgs) => {
-                Assert.IsNull(changingEventArgs);
-                Assert.AreEqual(changedValue, default);
-                Assert.AreSame(model, sender);
-                Assert.IsNotNull(sender);
-                Assert.IsNotNull(eventArgs);
+        model.PropertyChanging += (sender, eventArgs) => {
+            Assert.IsNull(changingEventArgs);
+            Assert.AreEqual(changedValue, default);
+            Assert.AreSame(model, sender);
+            Assert.IsNotNull(sender);
+            Assert.IsNotNull(eventArgs);
 
-                changingEventArgs = eventArgs;
-                changingValue = model.Data;
-            };
+            changingEventArgs = eventArgs;
+            changingValue = model.Data;
+        };
 
-            model.PropertyChanged += (sender, eventArgs) => {
-                Assert.IsNotNull(changingEventArgs);
-                Assert.AreEqual(changedValue, default);
-                Assert.AreSame(model, sender);
-                Assert.IsNotNull(sender);
-                Assert.IsNotNull(eventArgs);
+        model.PropertyChanged += (sender, eventArgs) => {
+            Assert.IsNotNull(changingEventArgs);
+            Assert.AreEqual(changedValue, default);
+            Assert.AreSame(model, sender);
+            Assert.IsNotNull(sender);
+            Assert.IsNotNull(eventArgs);
 
-                changedEventArgs = eventArgs;
-                changedValue = model.Data;
-            };
+            changedEventArgs = eventArgs;
+            changedValue = model.Data;
+        };
 
-            model.Data = 42;
+        model.Data = 42;
 
-            Assert.AreEqual(changingEventArgs?.PropertyName, "Data");
-            Assert.AreEqual(changingValue, 0);
-            Assert.AreEqual(changedEventArgs?.PropertyName, "Data");
-            Assert.AreEqual(changedValue, 42);
+        Assert.AreEqual(changingEventArgs?.PropertyName, "Data");
+        Assert.AreEqual(changingValue, 0);
+        Assert.AreEqual(changedEventArgs?.PropertyName, "Data");
+        Assert.AreEqual(changedValue, 42);
+    }
+
+    public class Person {
+        public string? Name { get; set; }
+    }
+
+    public class WrappedModelWithProperty : ObservableObject {
+        private Person Person { get; set; }
+
+        public WrappedModelWithProperty(Person person) => Person = person;
+
+        public Person PersonProxy => Person;
+
+        public string? Name {
+            get => Person.Name;
+            set => SetProperty(Person.Name, value, Person, (person, name) => person.Name = name);
         }
+    }
 
-        public class Person {
-            public string? Name { get; set; }
+    [TestMethod]
+    public void UpdateEventMethods_WrappedModelWithProperty_CorrectUpdateArguments() {
+        var model = new WrappedModelWithProperty(new Person { Name = "Marie" });
+
+        var changingEventArgs = default(PropertyChangingEventArgs);
+        var changingValue = default(string);
+
+        var changedEventArgs = default(PropertyChangedEventArgs);
+        var changedValue = default(string);
+
+        model.PropertyChanging += (sender, eventArgs) => {
+            Assert.AreSame(model, sender);
+
+            changingEventArgs = eventArgs;
+            changingValue = model.Name;
+        };
+
+        model.PropertyChanged += (sender, eventArgs) => {
+            Assert.AreSame(model, sender);
+
+            changedEventArgs = eventArgs;
+            changedValue = model.Name;
+        };
+
+        model.Name = "Alexander";
+
+        Assert.AreEqual(changingEventArgs?.PropertyName, "Name");
+        Assert.AreEqual(changingValue, "Marie");
+        Assert.AreEqual(changedEventArgs?.PropertyName, "Name");
+        Assert.AreEqual(changedValue, "Alexander");
+        Assert.AreEqual(model.PersonProxy?.Name, "Alexander");
+    }
+
+    public class WrappedModelWithField : ObservableObject {
+        private readonly Person _person;
+
+        public WrappedModelWithField(Person person) => _person = person;
+
+        public Person PersonWrapper => _person;
+
+        public string? Name {
+            get => _person.Name;
+            set => SetProperty(_person.Name, value, _person, (person, name) => person.Name = name);
         }
+    }
 
-        public class WrappedModelWithProperty : ObservableObject {
-            private Person Person { get; set; }
+    [TestMethod]
+    public void UpdateEventMethods_WrappedModelWithField_CorrectUpdateArguments() {
+        var model = new WrappedModelWithField(new Person { Name = "Marie" });
 
-            public WrappedModelWithProperty(Person person) => Person = person;
+        var changingEventArgs = default(PropertyChangingEventArgs);
+        var changingValue = default(string);
 
-            public Person PersonProxy => Person;
+        var changedEventArgs = default(PropertyChangedEventArgs);
+        var changedValue = default(string);
 
-            public string? Name {
-                get => Person.Name;
-                set => SetProperty(Person.Name, value, Person, (person, name) => person.Name = name);
-            }
-        }
+        model.PropertyChanging += (sender, eventArgs) => {
+            Assert.AreSame(model, sender);
 
-        [TestMethod]
-        public void UpdateEventMethods_WrappedModelWithProperty_CorrectUpdateArguments() {
-            var model = new WrappedModelWithProperty(new Person { Name = "Marie" });
+            changingEventArgs = eventArgs;
+            changingValue = model.Name;
+        };
 
-            var changingEventArgs = default(PropertyChangingEventArgs);
-            var changingValue = default(string);
+        model.PropertyChanged += (sender, eventArgs) => {
+            Assert.AreSame(model, sender);
 
-            var changedEventArgs = default(PropertyChangedEventArgs);
-            var changedValue = default(string);
+            changedEventArgs = eventArgs;
+            changedValue = model.Name;
+        };
 
-            model.PropertyChanging += (sender, eventArgs) => {
-                Assert.AreSame(model, sender);
+        model.Name = "Alexander";
 
-                changingEventArgs = eventArgs;
-                changingValue = model.Name;
-            };
-
-            model.PropertyChanged += (sender, eventArgs) => {
-                Assert.AreSame(model, sender);
-
-                changedEventArgs = eventArgs;
-                changedValue = model.Name;
-            };
-
-            model.Name = "Alexander";
-
-            Assert.AreEqual(changingEventArgs?.PropertyName, "Name");
-            Assert.AreEqual(changingValue, "Marie");
-            Assert.AreEqual(changedEventArgs?.PropertyName, "Name");
-            Assert.AreEqual(changedValue, "Alexander");
-            Assert.AreEqual(model.PersonProxy?.Name, "Alexander");
-        }
-
-        public class WrappedModelWithField : ObservableObject {
-            private readonly Person _person;
-
-            public WrappedModelWithField(Person person) => _person = person;
-
-            public Person PersonWrapper => _person;
-
-            public string? Name {
-                get => _person.Name;
-                set => SetProperty(_person.Name, value, _person, (person, name) => person.Name = name);
-            }
-        }
-
-        [TestMethod]
-        public void UpdateEventMethods_WrappedModelWithField_CorrectUpdateArguments() {
-            var model = new WrappedModelWithField(new Person { Name = "Marie" });
-
-            var changingEventArgs = default(PropertyChangingEventArgs);
-            var changingValue = default(string);
-
-            var changedEventArgs = default(PropertyChangedEventArgs);
-            var changedValue = default(string);
-
-            model.PropertyChanging += (sender, eventArgs) => {
-                Assert.AreSame(model, sender);
-
-                changingEventArgs = eventArgs;
-                changingValue = model.Name;
-            };
-
-            model.PropertyChanged += (sender, eventArgs) => {
-                Assert.AreSame(model, sender);
-
-                changedEventArgs = eventArgs;
-                changedValue = model.Name;
-            };
-
-            model.Name = "Alexander";
-
-            Assert.AreEqual(changingEventArgs?.PropertyName, "Name");
-            Assert.AreEqual(changingValue, "Marie");
-            Assert.AreEqual(changedEventArgs?.PropertyName, "Name");
-            Assert.AreEqual(changedValue, "Alexander");
-            Assert.AreEqual(model.PersonWrapper.Name, "Alexander");
-        }
+        Assert.AreEqual(changingEventArgs?.PropertyName, "Name");
+        Assert.AreEqual(changingValue, "Marie");
+        Assert.AreEqual(changedEventArgs?.PropertyName, "Name");
+        Assert.AreEqual(changedValue, "Alexander");
+        Assert.AreEqual(model.PersonWrapper.Name, "Alexander");
     }
 }
--- src/ImeSense.Helpers.Mvvm.Tests/Input/RelayCommandOfTTests.cs
+++ src/ImeSense.Helpers.Mvvm.Tests/Input/RelayCommandOfTTests.cs
@@ -2,114 +2,114 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using ImeSense.Helpers.Mvvm.Input;
 
-namespace ImeSense.Helpers.Mvvm.Tests.Input {
+namespace ImeSense.Helpers.Mvvm.Tests.Input;
+
+/// <summary>
+/// Test cases for <see cref="RelayCommand{T}" />
+/// </summary>
+[TestClass]
+public class RelayCommandOfTTests {
     /// <summary>
-    /// Test cases for <see cref="RelayCommand{T}" />
+    /// Asserts given action throws <see cref="ArgumentException" /> with specific parameter name
     /// </summary>
-    [TestClass]
-    public class RelayCommandOfTTests {
-        /// <summary>
-        /// Asserts given action throws <see cref="ArgumentException" /> with specific parameter name
-        /// </summary>
-        /// <param name="action">Input <see cref="Action"/> to invoke</param>
-        /// <param name="parameterName">Expected parameter name</param>
-        public static void ThrowArgumentException(Action action, string parameterName) {
-            var success = false;
+    /// <param name="action">Input <see cref="Action"/> to invoke</param>
+    /// <param name="parameterName">Expected parameter name</param>
+    public static void ThrowArgumentException(Action action, string parameterName) {
+        var success = false;
 
-            try {
-                action();
-            } catch (Exception exception) {
-                Assert.IsTrue(exception.GetType() == typeof(ArgumentException));
-                Assert.AreEqual(parameterName, ((ArgumentException) exception).ParamName);
+        try {
+            action();
+        } catch (Exception exception) {
+            Assert.IsTrue(exception.GetType() == typeof(ArgumentException));
+            Assert.AreEqual(parameterName, ((ArgumentException) exception).ParamName);
 
-                success = true;
-            }
-
-            Assert.IsTrue(success);
+            success = true;
         }
 
-        [TestMethod]
-        public void CanExecute_Null_ThrowArgumentNullException() {
-            var text = string.Empty;
-            Assert.ThrowsException<ArgumentNullException>(() => new RelayCommand<string>(source => text = source, null));
-        }
+        Assert.IsTrue(success);
+    }
 
-        [TestMethod]
-        public void Execute_String_CorrectStringAssignment() {
-            var text = string.Empty;
-            var command = new RelayCommand<string>(source => text = source);
-            Assert.IsTrue(command.CanExecute("Text"));
-            Assert.IsTrue(command.CanExecute(null));
+    [TestMethod]
+    public void CanExecute_Null_ThrowArgumentNullException() {
+        var text = string.Empty;
+        Assert.ThrowsException<ArgumentNullException>(() => new RelayCommand<string>(source => text = source, null));
+    }
 
-            ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
-            ThrowArgumentException(() => command.CanExecute(42), "parameter");
+    [TestMethod]
+    public void Execute_String_CorrectStringAssignment() {
+        var text = string.Empty;
+        var command = new RelayCommand<string>(source => text = source);
+        Assert.IsTrue(command.CanExecute("Text"));
+        Assert.IsTrue(command.CanExecute(null));
 
-            ThrowArgumentException(() => command.Execute(new object()), "parameter");
-            ThrowArgumentException(() => command.Execute(42), "parameter");
+        ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
+        ThrowArgumentException(() => command.CanExecute(42), "parameter");
 
-            var commandSender = default(object);
-            var commandEventArgs = default(EventArgs);
-            command.CanExecuteChanged += (sender, eventArgs) => {
-                commandSender = sender;
-                commandEventArgs = eventArgs;
-            };
-            command.NotifyCanExecuteChanged();
-            Assert.AreSame(commandSender, command);
-            Assert.AreSame(commandEventArgs, EventArgs.Empty);
+        ThrowArgumentException(() => command.Execute(new object()), "parameter");
+        ThrowArgumentException(() => command.Execute(42), "parameter");
 
-            command.Execute((object) "Hello");
-            Assert.AreEqual(text, "Hello");
+        var commandSender = default(object);
+        var commandEventArgs = default(EventArgs);
+        command.CanExecuteChanged += (sender, eventArgs) => {
+            commandSender = sender;
+            commandEventArgs = eventArgs;
+        };
+        command.NotifyCanExecuteChanged();
+        Assert.AreSame(commandSender, command);
+        Assert.AreSame(commandEventArgs, EventArgs.Empty);
 
-            command.Execute(null);
-            Assert.AreEqual(text, null);
-        }
+        command.Execute((object) "Hello");
+        Assert.AreEqual(text, "Hello");
 
-        [TestMethod]
-        public void CanExecute_DoubleString_CorrectStringsAssignment() {
-            var text = string.Empty;
-            var command = new RelayCommand<string>(source => text = source, source => source != null);
-            Assert.IsTrue(command.CanExecute("Text"));
-            Assert.IsFalse(command.CanExecute(null));
+        command.Execute(null);
+        Assert.AreEqual(text, null);
+    }
 
-            ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
-            ThrowArgumentException(() => command.CanExecute(42), "parameter");
+    [TestMethod]
+    public void CanExecute_DoubleString_CorrectStringsAssignment() {
+        var text = string.Empty;
+        var command = new RelayCommand<string>(source => text = source, source => source != null);
+        Assert.IsTrue(command.CanExecute("Text"));
+        Assert.IsFalse(command.CanExecute(null));
 
-            ThrowArgumentException(() => command.Execute(new object()), "parameter");
-            ThrowArgumentException(() => command.Execute(42), "parameter");
+        ThrowArgumentException(() => command.CanExecute(new object()), "parameter");
+        ThrowArgumentException(() => command.CanExecute(42), "parameter");
 
-            command.Execute((object) "Hello");
-            Assert.AreEqual(text, "Hello");
+        ThrowArgumentException(() => command.Execute(new object()), "parameter");
+        ThrowArgumentException(() => command.Execute(42), "parameter");
 
-            command.Execute(null);
-            Assert.IsNull(text);
-        }
+        command.Execute((object) "Hello");
+        Assert.AreEqual(text, "Hello");
 
-        [TestMethod]
-        public void CanExecute_Number_CorrectStringsAssignment() {
-            var n = 0;
-            var command = new RelayCommand<int>(i => n = i);
-            Assert.IsFalse(command.CanExecute(null));
+        command.Execute(null);
+        Assert.IsNull(text);
+    }
 
-            ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
-            ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
+    [TestMethod]
+    public void CanExecute_Number_CorrectStringsAssignment() {
+        var n = 0;
+        var command = new RelayCommand<int>(i => n = i);
+        Assert.IsFalse(command.CanExecute(null));
 
-            ThrowArgumentException(() => command.Execute(null), "parameter");
-            ThrowArgumentException(() => command.Execute("Hello"), "parameter");
-            ThrowArgumentException(() => command.Execute(3.14f), "parameter");
-        }
+        ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
+        ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
 
-        [TestMethod]
-        public void CanExecute_DoubleNumber_CorrectNumbersAssignment() {
-            var n = 0;
-            var command = new RelayCommand<int>(i => n = i, i => i > 0);
-            Assert.IsFalse(command.CanExecute(null));
+        ThrowArgumentException(() => command.Execute(null), "parameter");
+        ThrowArgumentException(() => command.Execute("Hello"), "parameter");
+        ThrowArgumentException(() => command.Execute(3.14f), "parameter");
+    }
 
-            ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
-            ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
+    [TestMethod]
+    public void CanExecute_DoubleNumber_CorrectNumbersAssignment() {
+        var n = 0;
+        var command = new RelayCommand<int>(i => n = i, i => i > 0);
+        Assert.IsFalse(command.CanExecute(null));
 
-            ThrowArgumentException(() => command.Execute(null), "parameter");
-            ThrowArgumentException(() => command.Execute("Hello"), "parameter");
-            ThrowArgumentException(() => command.Execute(3.14f), "parameter");
-        }
+        ThrowArgumentException(() => command.CanExecute("Hello"), "parameter");
+        ThrowArgumentException(() => command.CanExecute(3.14f), "parameter");
+
+        ThrowArgumentException(() => command.Execute(null), "parameter");
+        ThrowArgumentException(() => command.Execute("Hello"), "parameter");
+        ThrowArgumentException(() => command.Execute(3.14f), "parameter");
     }
 }
